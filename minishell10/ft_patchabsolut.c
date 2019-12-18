fec8363cac617428f353ce404be5f6472f9bc0c3
pikooli <53048735+pikooli@users.noreply.github.com>
2019-12-18 20:37:19 +0100

    Add files via upload

--- minishell10/ft_patchabsolut.c
+++ minishell10/ft_patchabsolut.c
@@ -16,13 +16,25 @@ char 	*ft_found_path(char **envp)
 char **ft_prepare_str(char *str)
 {
 	char **dest;
-	
+
+	if (!(str = ft_strtrim(str, " ")))
+		return (0);
 	if (!(str = ft_join("/", str)))
 		return (0);
 	if (!(dest = ft_split(str, ' ')))
 		return (0);
 	return (dest);
 }
+
+void	ft_test_path(char *str, char **envp)
+{		
+	char **arg;	
+	
+	if (!(arg = ft_split(str, ' ')))
+		return ;
+	execve(arg[0], &arg[0], envp);
+}
+
 int	ft_path(char *str, char **envp)
 {
 	char *path;
@@ -31,6 +43,7 @@ int	ft_path(char *str, char **envp)
 	char **arg;	
 	int i;
 
+	ft_test_path(str, envp);
 	if (!(path = ft_found_path(envp)))
 		return (1);
 	path++;
@@ -45,6 +58,6 @@ int	ft_path(char *str, char **envp)
 		if (!(arg[0] = ft_join(splitpath[i++], path)))
 			return (ft_free_2d(splitpath));
 		execve(arg[0], &arg[0], envp);
-	}	
+	}
 	return (1);
 }
